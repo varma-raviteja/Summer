d0ca962d11add00a356579cf986c58b8f99c1005
varma-raviteja <122059749+varma-raviteja@users.noreply.github.com>
2023-05-19 14:42:13 -0400

    Update Diameter of Binary Tree.java

--- Day_10/Diameter of Binary Tree.java
+++ Day_10/Diameter of Binary Tree.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution {    // O(N^2) O(N^2)
     int finalres=0;
 
     public static int maxDepth(TreeNode root) {
@@ -21,5 +21,28 @@ class Solution {
         finalres=Math.max(res,finalres);
         return finalres;
 
+    }
+}
+
+
+
+class Solution {
+    public int longestPath = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        longestPath(root);
+        return longestPath;
+    }
+    
+    public int longestPath(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        
+        int lheight = longestPath(root.left);
+        int rheight = longestPath(root.right);
+        
+        longestPath = Math.max(longestPath, lheight + rheight);
+        
+        return Math.max(lheight, rheight) + 1;
     }
 }
