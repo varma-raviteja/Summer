3281f7f3eead1548c111cce0703124780400832d
varma-raviteja <122059749+varma-raviteja@users.noreply.github.com>
2023-05-29 10:52:54 -0400

    Update Longest Consecutive Sequence.java

--- Day1/Longest Consecutive Sequence.java
+++ Day1/Longest Consecutive Sequence.java
@@ -34,3 +34,9 @@ class Solution {
 
 # TC: O(n); SC: O(n)
 # Accepted
+
+use set and move forward and backward while deleting them.
+
+Input: nums = [100,4,200,1,3,2]
+Output: 4
+Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
