c7c49f7771ad72d54801802c4617c16d6adf78ed
varma-raviteja <122059749+varma-raviteja@users.noreply.github.com>
2023-05-29 03:27:09 -0400

    Create Subtree of Another Tree.java

---
+++ Day_10/Subtree of Another Tree.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null)
+            return true;
+        if(p==null || q==null)
+            return false;
+        if(p.val!=q.val)
+            return false;
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+    }
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (subRoot == null || isSameTree(root, subRoot)) return true;
+        if (root == null) return false;
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+        
+    }
+}
