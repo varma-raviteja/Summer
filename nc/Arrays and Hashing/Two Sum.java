0452190d37734ee6bcee4a136725319d09d096be
varma-raviteja <122059749+varma-raviteja@users.noreply.github.com>
2023-06-06 07:43:22 -0400

    Create Two Sum.java

---
+++ nc/Arrays and Hashing/Two Sum.java
@@ -0,0 +1,50 @@
+// Brute Force - O(n*n), O(1)
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        // In case there is no solution, we'll just return null
+        return null;
+    }
+}
+
+
+// Two-pass Hash Table -O(n),O(n)
+
+public int[] twoSum(int[] nums, int target) {
+    HashMap<Integer,Integer> map =new HahsMap<>();
+    for(int i=0;i<byns.length;i++)
+    {
+        map.put(nums[i],i);
+    }
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+        }
+    }
+}
+
+
+// One - pass hash Table
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        // In case there is no solution, we'll just return null
+        return null;
+    }
+}
