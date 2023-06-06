504fdaedd0225f16d81f8faa9a2542ff56bc2e08
varma-raviteja <122059749+varma-raviteja@users.noreply.github.com>
2023-06-06 14:41:37 -0400

    Create Product of Array Except Self.java

---
+++ nc/Arrays and Hashing/Med/Product of Array Except Self.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int temp[]=new int[nums.length];
+        int prod=1;
+        temp[0]=1;
+        for(int i=1;i<nums.length;i++)
+        {
+            prod=prod*nums[i-1];
+            temp[i]=prod;
+            
+        }
+        prod=nums[nums.length-1];
+        for(int j=nums.length-2;j>=0;j--)
+        {
+            temp[j]=temp[j]*prod;
+            prod=prod*nums[j];
+        }
+        return temp;
+    }
+}
