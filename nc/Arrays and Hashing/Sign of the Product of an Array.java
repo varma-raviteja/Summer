c9ab7006badf5b5a25b6e6813f4f02c6eb17cb84
varma-raviteja <122059749+varma-raviteja@users.noreply.github.com>
2023-06-06 22:45:31 -0400

    Create Sign of the Product of an Array.java

---
+++ nc/Arrays and Hashing/Sign of the Product of an Array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        int neg=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==0)
+            return 0;
+            if(nums[i]<0)
+            neg++;
+        }
+
+
+        return (neg%2==0)? 1:-1;
+    }
+}
