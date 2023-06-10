19c0c0fc1538324d453c319cb8f94989327ba3a6
varma-raviteja <122059749+varma-raviteja@users.noreply.github.com>
2023-06-09 22:02:00 -0400

    Update Find K Closest Elements.java

--- nc/Sliding Window/Check/Find K Closest Elements.java
+++ nc/Sliding Window/Check/Find K Closest Elements.java
@@ -46,7 +46,25 @@ public List<Integer> findClosestElements(int[] arr, int k, int x) {
 
 
 
-
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        List<Integer> res = new ArrayList<>();
+        int l = 0;
+        int r = arr.length - k;
+        while(l < r){
+            int mid = l + (r - l)/2;
+            if(x - arr[mid] > arr[mid+k] - x){
+                l = mid+1;
+            }else{
+                r = mid;
+            }
+        }
+        for(int i=l; i<l+k; i++){
+            res.add(arr[i]);
+        }
+        return res;
+    }
+}
 
 
 
